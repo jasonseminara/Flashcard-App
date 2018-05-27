ad3ac9ae9581ddaed404a7d08df977950f7265a8
Andrew Bryner <bryner.and@gmail.com>
2018-05-27 14:00:32 -0400

    displaying json in react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      decks: []
+    }
+  }
+  componentDidMount() {
+    fetch(`${BASE_URL}/decks`)
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      decks: data.decks
+    }));
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>yo mama</h1>
+        <div>{JSON.stringify(this.state.decks)}</div>
       </div>
     );
   }
