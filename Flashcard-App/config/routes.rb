6996822e973df3d6012367ae3c0f6208349d39ff
Andrew Bryner <bryner.and@gmail.com>
2018-05-31 19:09:41 -0400

    added controllers

--- Flashcard-App/config/routes.rb
+++ Flashcard-App/config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
-    resources :decks
-    resources :cards
     resources :users
-    resources :favorites
-    resources :usercards
+    resources :quizzes
+    resources :questions
+    resources :instances
+    resources :answers
     post 'user_token' => 'user_token#create'
   end
 end
